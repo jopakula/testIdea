6062358d8c0fb7f965dc48dd94d0b07d8a64e50d
sergeiryzhkov <sergeyryzhkov2001@mail.ru>
2026-03-30 21:30:05 +0300

    Kata examples were added

--- src/TotalAmountOfPointsTests.kt
+++ src/TotalAmountOfPointsTests.kt
@@ -1,3 +1,9 @@
+fun main(){
+    println(points(listOf("1:0", "2:0", "3:0", "4:0", "2:1", "3:1", "4:1", "3:2", "4:2", "4:3")))
+    println(points(listOf("0:1", "0:2", "0:3", "0:4", "1:2", "1:3", "1:4", "2:3", "2:4", "3:4")))
+    println(points(listOf("1:0", "2:0", "3:0", "4:0", "2:1", "1:3", "1:4", "2:3", "2:4", "3:4")))
+}
+
 fun points(games: List<String>): Int {
     var score = 0
     games.forEach { game ->
